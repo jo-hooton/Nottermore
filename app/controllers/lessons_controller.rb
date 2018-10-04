9c2bf3300c66682c7fdfcf5d4f5de66eda78df86
github username <github email address>
2018-10-04 11:43:16 +0100

    enrolling student's classes - check-boxes+ showing classes on student show page

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -2,19 +2,26 @@ class LessonsController < ApplicationController
 
   def new
     @student = current_user
-    @subjects = Subject.all
     @lesson = Lesson.new
   end
 
   def create
     @student = current_user
-    @lesson = Lesson.new(lesson_params, student_id: @student.id)
-    if @lesson.valid?
-      @lesson.save
-      redirect_to @student.lessons
-    else
-      redirect_to new_lesson_path
+    @lessons = []
+    params[:lesson][:professor_id].each do |prof_id|
+      @lessons << Lesson.new(professor_id: prof_id, student_id: @student.id)
     end
+    # for each professor id - selected in the checkboxes, each prof_id is being added to @lessons empty array
+    # prof_id is what's being iterated over in the Lesson.new bit
+    @lessons.each do |lesson|
+      if lesson.valid?
+        lesson.save
+        # only if each lesson is valid, is it saved into DB
+      else
+        redirect_to new_lesson_path
+      en
+    end
+    redirect_to @student
   end
 
   def show
@@ -24,7 +31,7 @@ class LessonsController < ApplicationController
   private
 
   def lesson_params
-    params.require(:lesson).permit(:subject_id)
+    params.require(:lesson).permit(:professor_id => [])
   end
 
 end
